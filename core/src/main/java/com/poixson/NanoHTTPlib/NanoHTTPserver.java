114b06c53a1ba1fe37818a955ab3f65450a52803
Pete Lorenzo <lorenzo@poixson.com>
2014-01-03 19:03:18 -0500

    removed old ResponseException

--- core/src/main/java/com/poixson/NanoHTTPlib/NanoHTTPserver.java
+++ core/src/main/java/com/poixson/NanoHTTPlib/NanoHTTPserver.java
@@ -216,25 +216,6 @@ public class NanoHTTPserver extends NanoHTTPcommon {
 
 
 
-	public static final class ResponseException extends Exception {
-
-		private final Response.Status status;
-
-		public ResponseException(Response.Status status, String message) {
-			super(message);
-			this.status = status;
-		}
-
-		public ResponseException(Response.Status status, String message, Exception e) {
-			super(message, e);
-			this.status = status;
-		}
-
-		public Response.Status getStatus() {
-			return status;
-		}
-	}
-
 	/**
 	 * Default strategy for creating and cleaning up temporary files.
 	 */
